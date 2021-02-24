87ec81eb039887ae2f577dc704743d260c99da02
Kathrin Neyzberg <kneyzberg@gmail.com>
2021-02-23 16:07:19 -0800

    add event listener and ajax code for getting gif from giphy api

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log("Let's get this party started!");
+
+
+
+$("#submitBtn").on("click", getGif);
+
+async function getGif () {
+    let $searchTerm = $("#giphyInput").val();
+    let giphyResponse = await axios.get("https://api.giphy.com/v1/gifs/search", {params: {api_key: "o3XM3W1BHhTlDMKbn8ZRXz7XqWK4Pvju", q: $searchTerm}})
+    console.log("giphy resp =", giphyResponse);
+    let gif = randomGif(giphyResponse.data.data);   
+    console.log(gif, "gif");
+}
+function randomGif(giphyArr){
+    return giphyArr[Math.floor(Math.random() * giphyArr.length)];
+}
