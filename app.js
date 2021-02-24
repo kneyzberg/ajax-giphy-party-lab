1c08092a3ae1d5ccda0f9a70d805efb9cf834142
John <figurefactory@gmail.com>
2021-02-23 19:49:13 -0500

    steps 2-3

--- app.js
+++ app.js
@@ -3,14 +3,23 @@ console.log("Let's get this party started!");
 
 
 $("#submitBtn").on("click", getGif);
+$("#removeBtn").on("click", removeGifs);
 
 async function getGif () {
     let $searchTerm = $("#giphyInput").val();
     let giphyResponse = await axios.get("https://api.giphy.com/v1/gifs/search", {params: {api_key: "o3XM3W1BHhTlDMKbn8ZRXz7XqWK4Pvju", q: $searchTerm}})
-    console.log("giphy resp =", giphyResponse);
     let gif = randomGif(giphyResponse.data.data);   
-    console.log(gif, "gif");
+    appendGif(gif);
 }
+
 function randomGif(giphyArr){
     return giphyArr[Math.floor(Math.random() * giphyArr.length)];
+}
+
+function appendGif(gif) {
+    $("#giphy-holder").append(`<img src=${gif.images.fixed_height.url}>`);
+}
+
+function removeGifs() {
+    $("#giphy-holder").empty();
 }
